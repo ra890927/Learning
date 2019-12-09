73f5232dd98eaa10286ff49147562e262dd418b3
ra890927 <happy.890927410@gmail.com>
2019-12-09 23:27:11 +0800

    test

--- test/pJ.cpp
+++ test/pJ.cpp
@@ -1,160 +1,65 @@
 #include <bits/stdc++.h>
-#define MAX_N 500 + 1
+#define MAX_N 15 + 1
+#define INF 1e9
 using namespace std;
 
-int T, L, N, ans;
-struct Bin{
-    bool arr[MAX_N];
-    int cnt;
-    bool f;
-};
+int T, N, L;
+string s[MAX_N];
 
-Bin used[MAX_N], res;
-
-void OR(int k){
-    for(int i = 0; i < L; i++)
-        res.arr[i] |= used[k].arr[i];
+void OR(string &a, string &b){
+	for(int i = 0; i < L; i++){
+		if(a[i] == '1' || b[i] == '1')
+			a[i] = '1';
+	}
 }
 
-bool cmp(Bin a, Bin b){
-    if(a.cnt > b.cnt) return true;
-    else if(a.cnt == b.cnt)
-        for(int i = 0; i < L; i++){
-            if(a.arr[i]) return true;
-            if(b.arr[i]) return false;
-        }
-    else
-        return false;
+bool check(string s){
+	bool check = true;
+	for(int i = 0; i < L; i++){
+		if(s[i] == '0'){
+			check = false;
+			break;
+		}
+	}
+
+	return check;
 }
 
-void Cnt(int j){
-    int cnt = 0;
-    for(int i = 0; i < L; i++){
-        if(res.arr[i] != used[j].arr[i]) cnt++;
-    }
-    used[j].cnt = cnt;
-}
-
-void print(){
-            for(int i = 0; i < N; i++){
-            for(int j = 0; j < L; j++)
-                printf("%d", used[i].arr[j]);
-            puts("");
-        }
-}
-
-void print_r(){
-    for(int i = 0; i < L; i++)
-        cout << res.arr[i];
-    cout << endl;
-}
-
-void init(){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < L; j++)
-            used[i].arr[j] = false;
-        used[i].f = false;
-    }
-    for(int i = 0; i < L; i++)
-        res.arr[i] = false;
+void init(string &s){
+	for(int i = 0; i < L; i++)
+		s[i] = '0';
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    bool flag;
-    cin >> T;
-    while(T--){
-/*
-        for(int i = 0; i < N; i++)
-            memset(used[i].arr, 0, sizeof(used[i].arr));
-        memset(res.arr, 0, sizeof(res.arr));
-*/
-        init();
-        flag = true;
-        ans = 0;
-        cin >> L >> N;
-        string s;
+	cin >> T;
+	while(T--){
+		string res;
+		cin >> L >> N;
+		init(res);
+		cout << res << endl;
 
-        for(int i = 0; i < N; i++){
-            cin >> s;
-            for(int j = 0; j < L; j++){
-                used[i].arr[j] = (s[j] - '0');
-            }
-        }
+		for(int i = 0; i < N; i++)
+			cin >> s[i];
 
-        for(int i = 0; i < L; i++){
-            int cnt = 0, label = 0;
-            for(int j = 0; j < N; j++){
-                if(used[j].arr[i]){
-                    label = j;
-                    cnt++;
-                }
-            }
-            if(!cnt){
-                cout << -1 << endl;
-                flag = false;
-                break;
-            }
-            else if(cnt == 1){
-                OR(label);
-                used[label].f = true;
-                ans++;
-            }
-        }
-        if(flag){
-            bool ch = true;
-            while(ch){
-                //print_r();
-                ch = false;
-                for(int i = 0; i < N ;i++)
-                    Cnt(i);
-                sort(used, used + N, cmp);
-                for(int i = 0; i < N; i++){
-                    if(!used[i].f){
-                        OR(i);
-                        used[i].f = true;
-                        ans++;
-                        break;
-                    }
-                }
-                for(int i = 0; i < L; i++)
-                    if(!res.arr[i]) ch = true;
-            }
-            cout << ans << endl;
-        }
+		int ans = INF;
+		for(int i = 1; i < (N << 1); i++){
+			int cnt = 0;
+			for(int j = 0; j < N; j++){
+				if((i >> j) & 1){
+					cnt++;
+					OR(res, s[j]);
+					cout << res << endl;
+				}
+			}
 
+			if(check(res))
+				ans = min(ans, cnt);
 
-    }
+			init(res);
+		}
+
+		cout << ans << endl;
+	}
+
+	return 0;
 }
-/*
-5
-3 3
-100
-011
-111
-5 6
-10000
-01001
-01110
-00111
-10110
-00101
-6 7
-000010
-011000
-100100
-001000
-000010
-010000
-110001
-7 6
-1001001
-1001000
-0001101
-0010110
-0110011
-0100001
-2 1
-01
-*/
