86eab7748e628222325f36614b247c2c422fdfaa
ra890927 <happy.890927410@gmail.com>
2019-12-11 00:09:46 +0800

    Graph

---
+++ Graph/Single Source Shortest Path/Bellman_Ford.cpp
@@ -0,0 +1,36 @@
+struct Edge{
+	int from, to, cost;
+};
+
+int V, E;	//V是有幾個點，N是有幾條邊
+Edge ed[MAX_E];
+int d[MAX_V], parent[MAX_V];
+
+void negative_cycle(int s){
+	int next = parent[s];
+	cout << s;
+	
+	while(next != s){
+		cout << "->" << next;
+		next = parent[next];
+	}
+}
+
+void bellman_ford(int start){
+	fill(d, d + V + 1, INF);
+	memset(parent, 0, sizeof(parent));
+	
+	d[start] = 0;
+	parent[start] = start;
+	
+	for(int i = 1; i <= V - 1; i++){
+		for(int j = 0; j < E; j++){
+			Edge e = ed[j];
+			if(d[e.to] > d[e.from] + e.cost){
+				d[e.to] = d[e.from] + e.cost;
+				parent[e.to] = e.from;
+				if(i == V - 1) negative_cycle(e.to);
+			}
+		}
+	}
+}
