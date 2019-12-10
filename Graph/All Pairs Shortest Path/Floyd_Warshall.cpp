ce4946c7964a8a2f9399ca1c0c74af9cfacd60f1
ra890927 <happy.890927410@gmail.com>
2019-12-11 00:20:28 +0800

    Graph

--- Graph/All Pairs Shortest Path/Floyd_Warshall.cpp
+++ Graph/All Pairs Shortest Path/Floyd_Warshall.cpp
@@ -1,5 +1,10 @@
+#include <bits/stdc++.h>
+#define MAX_V 1000 + 1
+using namespace std;
+
 int V;
-int d[MAX_N][MAX_N];
+int d[MAX_V][MAX_V];
+
 void Floyd_Warshall(){
 	for(int k = 0; k < V; k++){
 		for(int i = 0; i < V; i++){
